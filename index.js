ff6a86806052f06fa57fd0f972ab0074070bb32a
Pankaj Patel <patel.pankaj@live.in>
2016-10-31 21:35:54 +0530

    make it look like server

--- index.js
+++ index.js
@@ -1,34 +1,65 @@
-var email, password, file;
-var system = require('system');
-var fs = require('fs');
-var page = require('webpage').create();
-var config = require('./config');
+const Hapi = require('hapi');
+var spawn = require('child_process').spawn;
 
-if (system.args.length === 1) {
-  console.log('Try to pass some args when invoking this script!');
-} else {
-  email = system.args[1]
-  password = system.args[2]
-  file = system.args[3] || "__op.html";
-}
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({
+  port: process.env.PORT || 8000
+});
 
-page.open(config.url, function (status) {
-  page.includeJs("https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js", function() {
-    page.evaluate(function(e, p) {
-      $("#email").val(e);
-      $("#password").val(p);
-      $("#submit-button").click();
-    }, email, password);
+// Add the route
+server.route({
+  method: 'GET',
+  path:'/',
+  handler: function (request, reply) {
+    return reply('PhantomBot!');
+  }
+});
+server.route({
+  method: 'GET',
+  path:'/hello',
+  handler: function (request, reply) {
+    return reply('hello world');
+  }
+});
 
-    setTimeout(function() {
-      var content = page.content;
-      try {
-        fs.write(file, content, 'w');
-      } catch(e) {
-        console.log(e);
+server.route({
+  method: 'GET',
+  path:'/do',
+  handler: function (request, reply) {
+    data = [];
+    p = spawn('phantomjs', ['phantomTask.js', process.env.LOGIN_EMAIL || process.argv[2], process.env.LOGIN_PASS  || process.argv[3]]);
+    function handleOutput(type, output) {
+      data.push({
+        type: type,
+        output: output
+      });
+      if( type === 'close' ){
+        reply(data);
       }
-      console.log('Exiting Now')
-      phantom.exit();
-    }, 30*1000);
-  });
+    }
+    p.stdout.on('data', function (data) {
+      // handleOutput('stdout: ' + data);
+      handleOutput('stdout', data);
+    });
+
+    p.stderr.on('data', function (data) {
+      // handleOutput('stderr: ' + data);
+      handleOutput('stderr', data);
+    });
+
+    p.on('close', function (code) {
+      // handleOutput('child process exited with code ' + code);
+      handleOutput('close', code);
+    });
+  }
+});
+
+// Start the server
+server.start((err) => {
+
+  if (err) {
+      throw err;
+  }
+  console.log('Server running at:', server.info.uri);
 });
